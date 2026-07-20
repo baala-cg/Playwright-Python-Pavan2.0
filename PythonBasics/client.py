1fb72e0a6232418aef8679b631d4a3794aaab239
Bala Krishna Jallipalli <baala.cg@gmail.com>
2026-07-20 21:09:08 +0530

    Removed static path

--- PythonBasics/client.py
+++ PythonBasics/client.py
@@ -1,29 +1,37 @@
 import fileoperations as fo
+import os
+
+print(os.getcwd())
+
 
 # Example usage
-file_path = "C:/Automation/automationFiles/myfile.txt"
+file_path = os.getcwd() + "\\files"
+file1 = file_path + "\\myfile.txt"
+file2 = file_path + "\\myfile1.txt"
 
 # Write to file
-fo.write_to_file(file_path, "Welcome to Python \n File Handling")
+fo.write_to_file(file1, "Welcome to Python \n File Handling")
 
 # Append to file
-fo.append_to_file(file_path, "\nThis line is appended.")
+fo.append_to_file(file1, "\nThis line is appended.")
 
 # Read file
-print(fo.read_file(file_path, "all"))
+print(fo.read_file(file1, "all"))
 
 # Rename file
-fo.rename_file(file_path, "C:/Automation/automationFiles/myfile1.txt")
+fo.rename_file(file1, file2)
 
 # Delete file
-fo.delete_file("C:/Automation/automationFiles/myfile1.txt")
+fo.delete_file(file2)
 
 # Directory operations
-dir_path = "C:/Automation/automationFiles/mydir"
+dir_path = file_path + "\\mydir"
 fo.create_directory(dir_path)
 print("Directory exists:", fo.check_directory_exists(dir_path))
-fo.rename_directory(dir_path, "C:/Automation/automationFiles/mydir1")
-fo.remove_directory("C:/Automation/automationFiles/mydir1", force=False)
+fo.rename_directory(dir_path, file_path + "\\mydir1")
+fo.remove_directory(file_path + "\\mydir1", force=False)
 
 # Current working directory
 print("CWD:", fo.get_current_working_directory())
+
+
